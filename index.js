ef7afb5ce60e670e2e67d8104879f75538704bda
danainjax <danainjax@gmail.com>
2021-05-18 13:16:45 -0400

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,26 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  time = time.split(":")[0];
+  if (time < 12) return "Good Morning";
+  if (time > 17) return "Good Evening";
+  else return "Good Afternoon";
+}
 /* Write your implementation of displayMessage() */
+// The `displayMessage` function should take one argument, a `String`.
+function displayMessage (message) {
+  document.getElementById('greeting').innerText = message
+}
+
+// When the function runs it should update the text inside the `#greeting` node
+// with the content of the argument.
+
+// It does not return anything.
